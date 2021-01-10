a395055177ddd91b21c0241df1347ca8e24c5133
Patrícia <Patricia@iMac-de-Patricia.local>
2021-01-10 00:22:48 -0300

    Updates

---
+++ precache-manifest.4308fbf27007fbfd311841938b1009c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de7a16423492869d4ae81ff72f1c14b",
+    "url": "/random-dogs/index.html"
+  },
+  {
+    "revision": "82b0a24811cacd84d951",
+    "url": "/random-dogs/static/css/main.538bd5d9.chunk.css"
+  },
+  {
+    "revision": "b23b0f878be985300136",
+    "url": "/random-dogs/static/js/2.2fc55f69.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/random-dogs/static/js/2.2fc55f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b0a24811cacd84d951",
+    "url": "/random-dogs/static/js/main.4f13960d.chunk.js"
+  },
+  {
+    "revision": "5318b90bc7ff0194ea46",
+    "url": "/random-dogs/static/js/runtime-main.051c7cb8.js"
+  }
+]);
